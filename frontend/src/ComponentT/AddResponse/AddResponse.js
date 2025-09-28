7882b9acd9302b3c7891c855a403e7780d8d3947
Amaya <tharushiamaya634@gmail.com>
2025-09-28 17:57:05 +0530

    TRespons-css added

--- frontend/src/ComponentT/AddResponse/AddResponse.js
+++ frontend/src/ComponentT/AddResponse/AddResponse.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
+import "./AddResponse.css";
 
 function AddResponse() {
   const { id } = useParams(); //  orderId from URL
